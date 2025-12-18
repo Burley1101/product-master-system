e6dc11215a35d1f39cae0c6974d551d1fe2afb9e
Burley1101 <me@burley.xin>
2025-12-19 06:35:35 +0800

    Implement Logger interface using logrus

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,120 @@
+// pkg/logger/logger.go
+package logger
 
+import (
+	"io"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Logger 日志接口
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	WithFields(fields map[string]interface{}) Logger
+	Sync() error
+}
+
+// logrusLogger 基于logrus的实现
+type logrusLogger struct {
+	entry *logrus.Entry
+}
+
+// NewLogger 创建新的日志实例
+func NewLogger(level, format, output string) (Logger, error) {
+	log := logrus.New()
+
+	// 设置日志级别
+	lvl, err := logrus.ParseLevel(level)
+	if err != nil {
+		lvl = logrus.InfoLevel
+	}
+	log.SetLevel(lvl)
+
+	// 设置日志格式
+	switch format {
+	case "json":
+		log.SetFormatter(&logrus.JSONFormatter{})
+	default:
+		log.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp:   true,
+			TimestampFormat: "2006-01-02 15:04:05",
+		})
+	}
+
+	// 设置输出
+	var out io.Writer = os.Stdout
+	if output != "stdout" && output != "" {
+		file, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			return nil, err
+		}
+		out = file
+	}
+	log.SetOutput(out)
+
+	return &logrusLogger{
+		entry: logrus.NewEntry(log),
+	}, nil
+}
+
+// 实现Logger接口的方法
+func (l *logrusLogger) Debug(args ...interface{}) {
+	l.entry.Debug(args...)
+}
+
+func (l *logrusLogger) Debugf(format string, args ...interface{}) {
+	l.entry.Debugf(format, args...)
+}
+
+func (l *logrusLogger) Info(args ...interface{}) {
+	l.entry.Info(args...)
+}
+
+func (l *logrusLogger) Infof(format string, args ...interface{}) {
+	l.entry.Infof(format, args...)
+}
+
+func (l *logrusLogger) Warn(args ...interface{}) {
+	l.entry.Warn(args...)
+}
+
+func (l *logrusLogger) Warnf(format string, args ...interface{}) {
+	l.entry.Warnf(format, args...)
+}
+
+func (l *logrusLogger) Error(args ...interface{}) {
+	l.entry.Error(args...)
+}
+
+func (l *logrusLogger) Errorf(format string, args ...interface{}) {
+	l.entry.Errorf(format, args...)
+}
+
+func (l *logrusLogger) Fatal(args ...interface{}) {
+	l.entry.Fatal(args...)
+}
+
+func (l *logrusLogger) Fatalf(format string, args ...interface{}) {
+	l.entry.Fatalf(format, args...)
+}
+
+func (l *logrusLogger) WithFields(fields map[string]interface{}) Logger {
+	return &logrusLogger{
+		entry: l.entry.WithFields(fields),
+	}
+}
+
+func (l *logrusLogger) Sync() error {
+	// logrus不需要特殊清理
+	return nil
+}
